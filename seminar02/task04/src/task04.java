3e52110e6e3b1a929513afcb559f9842935b0ab3
Denis Myagkov <tomich-84@mail.ru>
2023-03-09 16:44:43 +0300

    task04 corrected

--- seminar02/task04/src/task04.java
+++ seminar02/task04/src/task04.java
@@ -1,45 +1,54 @@
 //4*. К калькулятору из предыдущего дз добавить логирование.
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.*;
+
 public class task04 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Logger lg = Logger.getLogger(task04.class.getName());
+        lg.setLevel(Level.INFO);
+        FileHandler fh = new FileHandler("log.txt");
+        lg.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
 
         Scanner iScanner = new Scanner(System.in);
         System.out.print("Введите  a: ");
         int a = iScanner.nextInt();
         System.out.print("Введите операцию: ");
-        // iScanner.nextLine();
         char act = iScanner.next().charAt(0);
         if ((act != '+') && (act != '-') && (act != '*') && (act != '/')) {
-            System.out.println("Ошибка ввода операции!");
+            String msg = "Ошибка ввода операции!";
+            System.out.println(msg);
+            lg.log(Level.WARNING, msg);
+            return;
         }
         System.out.print("Введите b: ");
         int b = iScanner.nextInt();
         iScanner.close();
 
-        if (act == '+') {
-            int res = a + b;
-            System.out.printf("%d %c %d = %d", a, act, b, res);
-        }
-        if (act == '-') {
-            int res = a - b;
-            System.out.printf("%d %c %d = %d", a, act, b, res);
-        }
-        if (act == '*') {
-            int res = a * b;
-            System.out.printf("%d %c %d = %d", a, act, b, res);
-        }
-        if ((act == '/') && (b != 0)){
-            float res = (a * 1.0f / b);
-            System.out.printf("%d %c %d = %f", a, act, b, res);
-        }
-        else if ((act == '/') && (b == 0)){
-            System.out.println("Ошибка ввода! На 0 делить нельзя!");
+        if (act != '/') {
+            int res = 0;
+            if (act == '+') res = a + b;
+            if (act == '-') res = a - b;
+            if (act == '*') res = a * b;
+            String result = String.format("%d %c %d = %d\n", a, act, b, res);
+            System.out.println(result);
+            lg.log(Level.INFO, result);
         }
 
-
-
-
-
+        if (act == '/') {
+            if (b != 0) {
+                float res = (a * 1.0f / b);
+                String result = String.format("%d %c %d = %f\n", a, act, b, res);
+                System.out.println(result);
+                lg.log(Level.INFO, result);
+            } else {
+                System.out.println("Ошибка ввода! На 0 делить нельзя!");
+                lg.log(Level.WARNING, "Ошибка ввода! На 0 делить нельзя!");
+            }
+        }
     }
 }
