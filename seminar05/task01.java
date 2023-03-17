d795a8d31250084477e60e37bebd17e7704cc0f7
Denis Myagkov <tomich-84@mail.ru>
2023-03-17 16:28:28 +0300

    task01 sem05 added

---
+++ seminar05/task01.java
@@ -0,0 +1,27 @@
+// Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь
+// несколько телефонов.
+
+import java.util.*;
+
+public class task01 {
+    public static void main(String[] args) {
+        Map<String, ArrayList> directory = new HashMap<>();
+        while (true) {
+            System.out.print("Введите фамилию или \"exit\" для выхода: ");
+            Scanner iScanner = new Scanner(System.in);
+            String name = iScanner.nextLine();
+            if (name.equalsIgnoreCase("exit")) {
+                break;
+            }
+            System.out.print("Введите номера телефонов через пробел: ");
+            ArrayList<Integer> phoneNumbers = new ArrayList<>();
+            String[] numbers = iScanner.nextLine().split(" ");
+            for (String number : numbers) {
+                phoneNumbers.add(Integer.parseInt(number));
+            }
+            directory.put(name, phoneNumbers);
+        }
+        System.out.println(directory);
+    }
+}
+
